bdc78033bba4e360ab5d4ef5c675c8f2ee1ba151
Andrea Armando <andrea.armando@itiscueno.eu>
2023-09-15 13:54:32 +0200

    modifica

--- src/atom/Contact.jsx
+++ src/atom/Contact.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
 
 const Contact = () => {
+
+  const sendEmail = (e) =>{
+    e.preventDefault();
+
+    emailjs.sendForm('gmail', 'template_ldpotwv', e.target, 'SOYa_6Dcg40F0KWqiIv9q')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset();
+      console.log("mandata")
+
+  }
   return (
-    <div className='container_footer'></div>
+    <div className='container_footer'>
+      <div className='box_form'>
+
+        <p>Send me a message</p>
+        <form className='form' onSubmit={sendEmail}>
+          <div className='form--name'>
+            <label >Name:</label>
+            <input type = "text" placeholder='Name' name = "name"/>
+          </div>
+          <div className='form--email'>
+            <label >Email:</label>
+            <input type = "email" placeholder='Email' name = "email"/>
+          </div>
+          <div className='form--email'>
+            <label >Subject:</label>
+            <input type = "email" placeholder='Email' name = "email"/>
+          </div>
+          <div className='form--message'>
+            <label >Message:</label>
+            <input type = "text"  name = "message"/>
+          </div>
+        </form>
+        <div  type = "submit" className='btn_send'>Invia</div>
+      </div>
+    </div>
   )
 }
 
